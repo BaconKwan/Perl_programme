ab542bbb39c266feaafe1c64af9d26fce2098cff
guanpeikun <guanpeikun@luoda.site>
2014-09-15 16:49:01 +0800

    Mon Sep 15 16:49:01 HKT 2014

--- trends_analysis/draw_trend_analysis.pl
+++ trends_analysis/draw_trend_analysis.pl
@@ -109,6 +109,7 @@ foreach(@non)
 my $line = join ",", @sort;
 
 open CMD, "> $ARGV[2].r" or die $!;
+=cut
 print CMD "
 library(ggplot2)
 m <- read.table(\"$ARGV[2].tmp\", header=T, sep=\"\t\")
@@ -116,6 +117,76 @@ m\$cls <- factor(m\$cls, levels = c($line))
 ggplot(m, aes(x = xl, y= value, colour = pv)) + geom_line(size=1) + facet_wrap(~cls) + scale_color_manual(values = c(\"black\", \"red\")) + geom_rect(data=subset(m, pv==\"yes\"), aes(xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf, fill=pv), alpha = 0.05) + scale_fill_manual(values = c(\"blue\", \"blank\")) + theme_bw() + labs(x=\"\", y=\"\") + theme(legend.position = \"none\")
 ggsave(\"$ARGV[2].png\", width=7, height=7, dpi=300)
 ";
+=cut
+
+## add by guanpeikun code from gaochuan
+
+`cp $ARGV[1] $ARGV[1].tmp`;
+`sed -i "1d" $ARGV[1].tmp`;
+print CMD "
+m=read.table(\"$ARGV[1].tmp\", sep = \"\t\")
+profile_counts=nrow(m)
+rx=1
+while(rx*0.88*rx<=profile_counts)
+{
+	rx=rx+1
+}
+#rx
+#row_counts=as.numeric(args[2])
+row_counts=rx
+total_counts=0
+if(profile_counts%%row_counts==0)
+{
+	total_counts=profile_counts
+}
+if(profile_counts%%row_counts!=0)
+{
+	total_counts=(as.integer(profile_counts/row_counts)+1)*row_counts
+}
+mat=t(matrix(1:total_counts,nrow=row_counts))
+png(paste(\"$ARGV[2]\",\"sort_by_genes.png\",sep=\".\"), width = 800, height = 600, units = \"px\")
+layout(mat)
+colors=c(\"lightgreen\",\"lightblue\",\"lightpink\")
+palette(colors)
+ddd=1
+for(i in order(m[,4],decreasing=T))
+{
+	y=as.numeric(strsplit(as.character(m[i,2]),\",\")[[1]])
+	x=1:length(y)
+	max_y=max(abs(y))*1.15
+	par(mar=c(2,2,2,2))
+	plot(x,y,type=\"n\",xlim=c(min(x),max(x)),ylim=c(-max_y,max_y),xlab=\"\",ylab=\"\",xaxt=\"n\",yaxt=\"n\",main=paste(\"profile\",as.character(m[i,1]),\" : \",as.numeric(m[i,4]),\" genes\",sep=\"\"),cex.main=7/row_counts,las=2)
+	if(m[i,6]<0.05)
+	{
+		rect(-max_y*2, -max_y*2, 100, 100, col=ddd)
+		ddd=ddd+1
+	}
+	points(x,y,type=\"l\",lwd=7/row_counts*2.5)
+	box()
+}
+mat=t(matrix(1:total_counts,nrow=row_counts))
+png(paste(\"$ARGV[2]\",\"sort_by_pvalue.png\",sep=\".\"), width = 800, height = 600, units = \"px\")
+layout(mat)
+colors=c(\"lightgreen\",\"lightblue\",\"lightpink\")
+palette(colors)
+ddd=1
+for(i in order(m[,6]))
+{
+	y=as.numeric(strsplit(as.character(m[i,2]),\",\")[[1]])
+	x=1:length(y)
+	max_y=max(abs(y))*1.15
+	par(mar=c(2,2,2,2))
+	plot(x,y,type=\"n\",xlim=c(min(x),max(x)),ylim=c(-max_y,max_y),xlab=\"\",ylab=\"\",xaxt=\"n\",yaxt=\"n\",main=paste(\"profile\",as.character(m[i,1]),\" : \",as.numeric(m[i,4]),\" genes\",sep=\"\"),cex.main=7/row_counts,las=2)
+	if(m[i,6]<0.05)
+	{
+		rect(-max_y*2, -max_y*2, 100, 100, col=ddd)
+		ddd=ddd+1
+	}
+	points(x,y,type=\"l\",lwd=7/row_counts*2.5)
+	box()
+}
+dev.off()
+";
 
 `Rscript $ARGV[2].r`;
-`rm Rplots.pdf $ARGV[2].tmp $ARGV[2].r -rf`;
+`rm Rplots.pdf $ARGV[2].tmp $ARGV[2].r $ARGV[1].tmp -rf`;
---
+++ trends_analysis/plot_sort_by_pvalue.R
@@ -0,0 +1,42 @@
+args<-commandArgs(T)
+data=read.table(args[1], sep="\t")
+profile_counts=nrow(data)
+rx=1
+while(rx*0.88*rx<=profile_counts)
+{
+	rx=rx+1
+}
+#rx
+#row_counts=as.numeric(args[2])
+row_counts=rx
+total_counts=0
+if(profile_counts%%row_counts==0)
+{
+	total_counts=profile_counts
+}
+if(profile_counts%%row_counts!=0)
+{
+	total_counts=(as.integer(profile_counts/row_counts)+1)*row_counts
+}
+mat=t(matrix(1:total_counts,nrow=row_counts))
+pdf(paste(args[1],"sort_by_pvalue.pdf",sep="."),10,8)
+layout(mat)
+colors=c("lightgreen","lightblue","lightpink")
+palette(colors)
+ddd=1
+for(i in order(data[,6]))
+{
+	y=as.numeric(strsplit(as.character(data[i,2]),",")[[1]])
+	x=1:length(y)
+	max_y=max(abs(y))*1.15
+	par(mar=c(2,2,2,2))
+	plot(x,y,type="n",xlim=c(min(x),max(x)),ylim=c(-max_y,max_y),xlab="",ylab="",xaxt="n",yaxt="n",main=paste("profile",as.character(data[i,1])," : ",as.numeric(data[i,4])," genes",sep=""),cex.main=7/row_counts,las=2)
+	if(data[i,6]<0.05)
+	{
+		rect(-max_y*2, -max_y*2, 100, 100, col=ddd)
+		ddd=ddd+1
+	}
+	points(x,y,type="l",lwd=7/row_counts*2.5)
+	box()
+}
+dev.off()
