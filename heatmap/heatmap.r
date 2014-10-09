ac4df2b4fd7a857d72cd4ade83551f302e107fb8
guanpeikun <guanpeikun@linux-6wm1.site>
2014-10-09 15:18:47 +0800

    Thu Oct  9 15:18:47 HKT 2014

--- heatmap/heatmap.r
+++
@@ -1,9 +0,0 @@
-library(pheatmap)
-args<-commandArgs(T)
-data = read.table(args[1], header = TRUE, sep = "\t", check.name = FALSE)
-rownames(data) = data[,1]
-data = data[,2:length(data[1,])]
-id <- paste(args[1], ".pdf", sep="")
-pdf(file = id, height = 10)
-pheatmap(data, cluster_rows = FALSE, cluster_cols = FALSE, cellwidth = 70, cellheight = 10)
-dev.off()
