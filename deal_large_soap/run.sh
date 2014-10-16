0f93f5b732346eb0c9b27e1a5301e6b3986d2a96
guanpeikun <guanpeikun@linux-6wm1.site>
2014-10-16 16:42:39 +0800

    Thu Oct 16 16:42:39 HKT 2014

--- deal_large_soap/run.sh
+++ deal_large_soap/run.sh
@@ -1,2 +1,2 @@
 perl /home/guanpeikun/bin/deal_large_soap/cutSoap.pl Pinus_bungeana.PESoapAlign.gz 100
-perl /home/guanpeikun/bin/deal_large_soap/PE2SE.pl Pinus_bungeana.PESoapSingle.gz Pinus_bungeana.PESoapAlign
+perl /home/guanpeikun/bin/deal_large_soap/PE2SE.pl Pinus_bungeana.PESoapSingle.gz Pinus_bungeana.PESoapAlign.list
