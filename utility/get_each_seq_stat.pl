c44d49144b7eb58d61db64bcf24fbe8fbffaae86
guanpeikun <guanpeikun@luoda.site>
2015-01-05 14:02:11 +0800

    Mon Jan  5 14:02:11 HKT 2015

---
+++ utility/get_each_seq_stat.pl
@@ -0,0 +1,29 @@
+#! /usr/bin/perl
+use strict;
+use utf8;
+use warnings;
+use Bio::SeqIO;
+use Bio::Seq;
+
+die"perl $0 <fa> \n" unless(@ARGV eq 1);
+
+my $in = Bio::SeqIO->new(-file=>"< $ARGV[0]", -format=>"fasta");
+
+print "Seq ID\tA%\tG%\tC%\tT%\tN%\tX%\n";
+while (my $seq = $in->next_seq()){
+	my $len = $seq->length;
+	my $a = $seq->seq =~ tr/Aa/Aa/;
+	my $g = $seq->seq =~ tr/Gg/Gg/;
+	my $c = $seq->seq =~ tr/Cc/Cc/;
+	my $t = $seq->seq =~ tr/Tt/Tt/;
+	my $n = $seq->seq =~ tr/Nn/Nn/;
+	my $x = 100 * ($len - $a - $g - $c - $t - $n) / $len;
+
+	$a = 100 * $a / $len;
+	$g = 100 * $g / $len;
+	$c = 100 * $c / $len;
+	$t = 100 * $t / $len;
+	$n = 100 * $n / $len;
+
+	printf("%s\t%.2f%%\t%.2f%%\t%.2f%%\t%.2f%%\t%.2f%%\t%.2f%%\n", $seq->id, $a, $g, $c, $t, $n, $x);
+}
